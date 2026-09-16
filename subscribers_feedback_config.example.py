e606c9cd309a95a7af04e23a08a400a36270174d
Ibndaud <107104641+Ibndaud@users.noreply.github.com>
2026-09-16 13:02:39 +0300

    created subscribers_feedback_config.example.py

---
+++ subscribers_feedback_config.example.py
@@ -0,0 +1,8 @@
+# PostgreSQL connection parameters
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': '5432',
+    'database': 'example_de',
+    'user': 'example_user',
+    'password': 'example_password',
+    }
